4ad537909541b49053ede9d707b773e1bcd06463
Truong <ty339178@toyo-local.co.jp>
2019-06-26 15:08:56 +0700

    continues learn

--- src/Dog/redux/action.js
+++ src/Dog/redux/action.js
@@ -1,32 +1,56 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, take, call, put, delay, race } from "redux-saga/effects";
 
 import {
   API_CALL_REQUEST,
   API_CALL_SUCCESS,
-  API_CALL_FAILURE,
-} from './reducer';
+  API_CALL_FAILURE
+} from "./reducer";
 
-import axios from 'axios';
+import axios from "axios";
 
-export default function* watcherSaga() {
-  yield takeLatest(API_CALL_REQUEST, workerSaga);
-}
-
-// function that makes the api request and returns a Promise for response
 const fetchDog = () => {
   return axios({
-    method: 'get',
-    url: "https://dog.ceo/api/breeds/image/random",
-  })
-}
+    method: "get",
+    url: "https://dog.ceo/api/breeds/image/random"
+  });
+};
 
-// worker saga: makes the api call when watcher saga sees the action
 function* workerSaga() {
   try {
     const { data } = yield call(fetchDog);
     const dog = data.message;
-    yield put({ type: API_CALL_SUCCESS, dog});
+    yield put({ type: API_CALL_SUCCESS, dog });
   } catch (err) {
-    yield put({ type: API_CALL_FAILURE, err});
+    yield put({ type: API_CALL_FAILURE, err });
+  }
+}
+
+// watcher saga with takeLatest Effects
+// export default function* watcherSagaTakeLatest() {
+//   yield takeLatest(API_CALL_REQUEST, workerSaga);
+// }
+
+// watcher saga with take Effects
+// export default function* watcherSagaTake() {
+//   while (yield take(API_CALL_REQUEST)) {
+//     yield call(workerSaga);
+//   }
+// }
+
+// watcher saga with Race Effect
+export default function* watcherSagaRace() {
+  yield take(API_CALL_REQUEST);
+  let finished = false;
+  while (!finished) {
+    const { url, timeout } = yield race({
+      url: call(fetchDog),
+      timeout: delay(5000)
+    });
+    const dog = url.data.message;
+    if (!timeout) {
+      finished = true;
+      yield put({ type: API_CALL_SUCCESS, dog });
+    }
+    console.log(finished, dog, timeout);
   }
 }
